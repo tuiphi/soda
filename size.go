671976b703e2b708e381d932f4ba769ae6b20905
metafates <vvz.dev@gmail.com>
2024-01-05 20:22:35 +0300

    feat: add some size methods

--- size.go
+++ size.go
@@ -1,6 +1,9 @@
 package soda
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Size struct {
 	Width, Height int
@@ -8,4 +11,31 @@ type Size struct {
 
 func (s Size) String() string {
 	return fmt.Sprint(s.Width, "x", s.Height)
+}
+
+func (s Size) SplitHorizontal(leftRatio float64) (Size, Size) {
+	width := s.Width
+	left := int(math.Round(float64(width) * leftRatio))
+	right := width - left
+
+	return Size{
+			Width:  left,
+			Height: s.Height,
+		}, Size{
+			Width:  right,
+			Height: s.Height,
+		}
+}
+
+func (s Size) SplitVertical(topRatio float64) (Size, Size) {
+	left, right := s.swapped().SplitHorizontal(topRatio)
+
+	return left.swapped(), right.swapped()
+}
+
+func (s Size) swapped() Size {
+	return Size{
+		Width:  s.Height,
+		Height: s.Width,
+	}
 }
